f48d6cb36ed33faeb8dd8d64b10872236c3b4850
MaLiRu9 <lolo_fosils_93@yahoo.es>
2022-07-12 13:59:29 +0200

    App Fin de Clase 20220712

--- app/src/main/java/com/agaprog/notes/json/JsonService.kt
+++ app/src/main/java/com/agaprog/notes/json/JsonService.kt
@@ -1,10 +1,37 @@
 package com.agaprog.notes.json
 
+import android.content.Context
 import android.util.Log
+import com.agaprog.notes.list.ListService
+import com.agaprog.notes.list.item.Item
+import org.json.JSONArray
+import java.io.File
 
-class JsonService {
+class JsonService(val context: Context) {
+
     fun createJsonFile(name: String, copyExample: Boolean) {
         Log.d("Debug", "CREA UN NUEVO JSON: " + name)
         //TODO crear un nuevo fichero JSON
+
+        var list = mutableListOf<Item>()
+
+        if (copyExample) {
+            list = getListFromExample()
+        }
+        val folder = File(context.filesDir,"jsons")
+        if (!folder.exists()) {
+            folder.mkdir()
+        }
+        val file = File(folder , name +".json")
+        file.createNewFile()
     }
+
+    private fun getListFromExample():MutableList<Item> {
+        val file = context.assets.open("example.json")
+        val jsonArray = JSONArray(context.assets.open("example.json").bufferedReader())
+        it.readText()
+    })
+    val list = ListService(context).parseJson(jsonArray)
+    return list
+}
 }
